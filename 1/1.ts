f0c34b22e1fdf876b15f3f44efea523624d6ec8f
Arne Martin Aurlien <am@aurlien.net>
2020-12-07 17:14:32 +0100

    yolo

--- 1/1.ts
+++ 1/1.ts
@@ -1,32 +1,13 @@
-import { inputLines, returnAndLog } from '../common'
+import { inputLines, product } from '../common'
 
 const input = inputLines().map(line => parseInt(line, 10)).sort((a, b) => a - b)
-
-type FwcCallback<T, U> = (value: T, i: number) => [found: boolean, context: U]
-type FwcResponse<T, U> = [value: T, context: U] | undefined
-
-declare global {
-  interface Array<T> {
-    findWithContext<U>(callback: FwcCallback<T, U>): FwcResponse<T, U>
-  }
-}
-
-Array.prototype.findWithContext = function<T, U>(callback: FwcCallback<T, U>): FwcResponse<T, U> {
-  let i = 0
-  for (const v of this) {
-    const [found, context] = callback(v, i++)
-    if (found) {
-      return [v, context]
-    }
-  }
-}
 
 const part1numbers = input.findWithContext((n, i) => {
   const v = input.slice(i + 1).find(n2 => n + n2 == 2020)
   return [v != null, v]
 })
 
-console.log('Part 1:', part1numbers[0] * part1numbers[1])
+console.log('Part 1:', product(part1numbers))
 
 const part2numbers = input.findWithContext((n, i) => {
   const v = input.slice(i + 1).findWithContext((n2, j) => {
@@ -36,4 +17,4 @@ const part2numbers = input.findWithContext((n, i) => {
   return [v != null, v]
 }).flat()
 
-console.log('Part 2:', part2numbers[0] * part2numbers[1] * part2numbers[2])
+console.log('Part 2:', product(part2numbers))
--- common.ts
+++ common.ts
@@ -2,6 +2,26 @@ import { readFileSync } from 'fs'
 
 process.chdir(require.main.path)
 
+type FwcCallback<T, U> = (value: T, i: number) => [found: boolean, context: U]
+type FwcResponse<T, U> = [value: T, context: U] | undefined
+
+declare global {
+  interface Array<T> {
+    findWithContext<U>(callback: FwcCallback<T, U>): FwcResponse<T, U>
+  }
+}
+
+Array.prototype.findWithContext = function<T, U>(callback: FwcCallback<T, U>): FwcResponse<T, U> {
+  let i = 0
+  for (const v of this) {
+    const [found, context] = callback(v, i++)
+    if (found) {
+      return [v, context]
+    }
+  }
+}
+
+
 export const inputLines = (splitWith: RegExp = /\n/) => readFileSync('input.txt').toString().split(splitWith)
 
 export const xor = (a: boolean, b: boolean) => (a && !b) || (!a && b)
@@ -9,13 +29,6 @@ export const xor = (a: boolean, b: boolean) => (a && !b) || (!a && b)
 export const within = (min: number, max: number) => (num: string | number): boolean => {
   const n = typeof num == 'number' ? num : parseInt(num, 10)
   return n != null && n >= min && n <= max
-}
-
-export const returnAndLog = (ret: any = true) => (...args: any[]) => {
-  if (ret) {
-    console.log(...args)
-  }
-  return ret
 }
 
 export const chars = (s: string) => s.replace(/\n/g, '').split('')
@@ -26,3 +39,5 @@ export const frequencies = <T>(list: T[]) =>
   list.reduce((freqs, e) => freqs.set(e, (freqs.get(e) || 0) + 1), new Map<T, number>())
 
 export const pluck = (key: string) => (o: { [key: string]: any }) => o[key]
+
+export const product = (vals: number[]): number => eval(vals.join('*'))
