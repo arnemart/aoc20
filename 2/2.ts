500187fa976f36f1936924a796a6b36b7a7ee657
Arne Martin Aurlien <am@aurlien.net>
2020-12-07 16:25:58 +0100

    yolo

--- 2/2.ts
+++ 2/2.ts
@@ -7,17 +7,17 @@ interface Pw {
   pw: string
 }
 
-const reg = /^(\d+)-(\d+) (\w): (\w+)$/
+const pwreg = /^(\d+)-(\d+) (\w): (\w+)$/
 
 const valid1 = ({pw, min, max, letter}: Pw) => within(min, max)(chars(pw).filter(l => l == letter).length)
 
 const valid2 = ({pw, min, max, letter}: Pw) => xor(pw.charAt(min - 1) == letter, pw.charAt(max - 1) == letter)
 
-const input: Pw[] = inputLines().map(line => line.match(reg)).map(matches => ({
-  min: parseInt(matches[1]),
-  max: parseInt(matches[2]),
-  letter: matches[3],
-  pw: matches[4]
+const input: Pw[] = inputLines().map(l => l.match(pwreg)).map(([_, min, max, letter, pw]) => ({
+  min: parseInt(min),
+  max: parseInt(max),
+  letter,
+  pw
 }))
 
 console.log('Part 1:', input.filter(valid1).length)
