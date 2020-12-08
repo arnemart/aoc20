9f2a9f374b23566b1222fbafda42d3615688967b
Arne Martin Aurlien <am@aurlien.net>
2020-12-08 10:42:35 +0100

    yolo

--- 8/8.ts
+++ 8/8.ts
@@ -1,62 +1,7 @@
 import { inputLines } from '../common'
+import { parseProgram, runProgram, Program } from '../vm'
 
-interface Instruction {
-  type: 'acc' | 'jmp' | 'nop',
-  val: number
-}
-
-type Program = Instruction[]
-
-interface Machine {
-  program: Program
-  counter: number
-  accumulator: number
-  terminated: boolean
-}
-
-const insreg = /^(acc|jmp|nop) ([+-]\d+)$/
-
-const input: Program = inputLines().map(line => line.match(insreg)).map(matches => ({
-  type: matches[1],
-  val: parseInt(matches[2])
-} as Instruction))
-
-const step = (m: Machine): Machine => {
-  if (m.counter == m.program.length) {
-    return { ...m, terminated: true }
-  }
-
-  const ins = m.program[m.counter]
-  switch(ins.type) {
-    case 'acc': return {
-      ...m,
-      counter: m.counter + 1,
-      accumulator: m.accumulator + ins.val
-    }
-    case 'jmp': return {
-      ...m,
-      counter: m.counter + ins.val
-    }
-    default: return {
-      ...m,
-      counter: m.counter + 1
-    }
-  }
-}
-
-const runProgram = (program: Program, seenInstructions = new Set<number>(), machine: Machine = {
-  program: program,
-  counter: 0,
-  accumulator: 0,
-  terminated: false
-}): Machine => {
-  const m = step(machine)
-  if (m.terminated || seenInstructions.has(m.counter)) {
-    return m
-  } else {
-    return runProgram(program, seenInstructions.add(m.counter), m)
-  }
-}
+const input = parseProgram(inputLines())
 
 console.log('Part 1:', runProgram(input).accumulator)
 
