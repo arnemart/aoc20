293633ef62ffd660146b005c9e718ea400e9c2be
Arne Martin Aurlien <am@aurlien.net>
2020-12-10 10:36:09 +0100

    yolo

---
+++ 10/10.ts
@@ -0,0 +1,19 @@
+import { inputLines } from '../common'
+
+const joltages = inputLines().numbers().sortNumeric()
+joltages.push(joltages[joltages.length - 1] + 3)
+
+const { ones, threes, runs } = joltages.reduce(({ ones, threes, runs, currentRun }, joltage, i) => {
+  const diff = joltage - (joltages[i - 1] || 0)
+  return {
+    ones: ones + (diff == 1 ? 1 : 0),
+    threes: threes + (diff == 3 ? 1 : 0),
+    runs: runs.concat(diff != 1 && currentRun > 1 ? currentRun + 1 : []),
+    currentRun: diff == 1 ? currentRun + 1 : 0
+  }
+}, { ones: 0, threes: 0, runs: [], currentRun: 0 })
+
+const validPermutations = runs.map(n => Math.pow(2, n - 2) - (n >= 5 ? 1 : 0)).product()
+
+console.log('Part 1:', ones * threes)
+console.log('Part 2:', validPermutations)
