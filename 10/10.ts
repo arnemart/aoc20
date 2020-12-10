a4bd222fb832cc43721f879b86732fa544e988f0
Arne Martin Aurlien <am@aurlien.net>
2020-12-10 11:22:44 +0100

    yolo

--- 10/10.ts
+++ 10/10.ts
@@ -7,10 +7,10 @@ const { ones, threes, runs } = joltages
 .zip([0, ...joltages])
 .map(([j, p]) => j - p)
 .reduce(({ ones, threes, runs, currentRun }, diff) => ({
-  ones: ones + (diff == 1 ? 1 : 0),
-  threes: threes + (diff == 3 ? 1 : 0),
-  runs: runs.concat(diff != 1 && currentRun > 1 ? currentRun + 1 : []),
-  currentRun: diff == 1 ? currentRun + 1 : 0
+  ones: (diff == 1 ? ones + 1 : ones),
+  threes: (diff == 3 ? threes + 1 : threes),
+  runs: (diff > 1 && currentRun > 1 ? [...runs, currentRun + 1] : runs),
+  currentRun: (diff == 1 ? currentRun + 1 : 0)
 }), { ones: 0, threes: 0, runs: [] as number[], currentRun: 0 })
 
 const validPermutations = runs.map(n => Math.pow(2, n - 2) - (n >= 5 ? 1 : 0)).product()
