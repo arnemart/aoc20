0d40836b5682ecebf34136a116a61240f41d6dd0
Arne Martin Aurlien <am@aurlien.net>
2020-12-15 10:32:22 +0100

    yolo

--- 15/15.ts
+++ 15/15.ts
@@ -1,12 +1,12 @@
-import { $, inputLines, last, map, numbers, pluck, range, reduce, slice } from '../common'
+import { $, inputLines, last, numbers, pluck, range, reduce, slice } from '../common'
 
 const input = $(inputLines(/,/), numbers())
 
 const getFinalNumber = (nth: number, input: number[]) => $(
   range(input.length, nth),
-  reduce(({ mp, num: prev }, turn) => ({
-    num: (mp.has(prev) ? turn - mp.get(prev) : 0),
-    mp: mp.set(prev, turn)
+  reduce(({ mp, num }, turn) => ({
+    num: turn - mp.get(num) || 0,
+    mp: mp.set(num, turn)
   }), {
     num: $(input, last),
     mp: $(input, slice(0, -1), reduce((m, p, n) => m.set(p, n + 1), new Map<number, number>()))
