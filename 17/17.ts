8f50b33cbe9fa6c09784b5958a3b70a8101451c5
Arne Martin Aurlien <am@aurlien.net>
2020-12-18 00:48:33 +0100

    yolo

--- 17/17.ts
+++ 17/17.ts
@@ -1,21 +1,20 @@
-import { $, cond, count, fillArray, filter, flatten, getIn, inputLines, is, map, pipe, pluck, range, repeat, split, within, memoize, reduce } from '../common'
+import { $, cond, count, fillArray, filter, flatten, getIn, inputLines, is, map, pluck, range, repeat, split, within, memoize, reduce, add } from '../common'
 
 interface Grid extends Array<Grid | string> {}
-type Neighbour = number[]
 
-const initialState: Grid = [$(inputLines(), map(pipe(split())))]
+const initialState: Grid = [$(inputLines(), map(split()))]
 const initialState4d: Grid = [initialState]
 
 const coords = [-1, 0, 1]
 
-const generateNeighbours = (dims: number): Neighbour[] => dims <= 1 ?
+const generateNeighbours = (dims: number): number[][] => dims <= 1 ?
   $(coords, map(c => [c])) :
   $(generateNeighbours(dims - 1),
     map(n => $(coords,
       map(c => [...n, c]))),
     flatten())
 
-const neighbours: (dims: number) => Neighbour[] = memoize((dims) => $(dims,
+const neighbours: (dims: number) => number[][] = memoize((dims) => $(dims,
   generateNeighbours,
   filter(reduce((allNonZero: boolean, n) => (allNonZero || n != 0), false))))
 
@@ -28,10 +27,10 @@ const expand = (grid: Grid): Grid => {
   if (dims == 1) {
     return ['.', ...grid, '.']
   }
-  const sizes: number[] = $(range(dims - 1), map(n => fillArray(n + 1, 0)), map(ns => $(grid, getIn(...ns), pluck('length'), n => n + 2)))
+  const sizes: number[] = $(range(dims - 1), map(n => fillArray(n + 1, 0)), map(ns => $(grid, getIn(...ns), pluck('length'), add(2))))
   return [
     emptyGrid(sizes),
-    ...$(grid, map(part => expand(part as Grid))),
+    ...$(grid, map(expand)),
     emptyGrid(sizes)
   ]
 }
@@ -49,7 +48,7 @@ const step = (initialGrid: Grid): Grid => {
   const expandedGrid = expand(initialGrid)
   const recur = (grid: Grid, coords: number[] = []): Grid => countDimensions(grid) <= 1 ?
     $(grid, map((cell: string, c) => alive(cell, countActiveNeighbours(expandedGrid, [...coords, c])))) :
-    $(grid, map((part, c) => recur(part as Grid, [...coords, c])))
+    $(grid, map((part: Grid, c) => recur(part, [...coords, c])))
 
   return recur(expandedGrid)
 }
--- common.ts
+++ common.ts
@@ -123,3 +123,4 @@ export const spy: <T>(v: T) => T = spyWith(console.log)
 export const leftPad = (length: number, padWith: string) => (s: string): string => Array.from(Array(Math.max(0, length - s.length + 1))).join(padWith) + s
 export const not = <T>(fn: (v: T) => boolean) => (v: T): boolean => !fn(v)
 export const repeat = <T>(n: number, fn: (v: T) => T) => (v: T): T => n == 0 ? v : repeat(n - 1, fn)(fn(v))
+export const add = (n: number) => (v: number): number => n + v
