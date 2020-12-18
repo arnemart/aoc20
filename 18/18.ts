d146d13073d0e0bb9f70d6939c34571e8acd1424
Arne Martin Aurlien <am@aurlien.net>
2020-12-18 12:38:20 +0100

    yolo

---
+++ 18/18.ts
@@ -0,0 +1,23 @@
+import { $, inputLines, map, sum, numbers, pipe } from '../common'
+
+const input = inputLines()
+
+const parenReg = /\(([^()]+)\)/g
+const calcReg = /^\d+\s*(\+|\*)\s*\d+/
+const plusReg = /\d+\s*\+\s*\d+/
+
+const calculateFromLeft = (line: string): string => calcReg.test(line) ?
+  calculateFromLeft(line.replace(calcReg, eval)) :
+  line
+
+const replaceParens = (calcFn: (s: string) => string) => (line: string): string => /\(/.test(line) ?
+  replaceParens(calcFn)(line.replace(parenReg, (_, match) => calcFn(match))) :
+  calcFn(line)
+
+console.log('Part 1:', $(input, map(replaceParens(calculateFromLeft)), numbers(), sum))
+
+const calculatePlusFirst = (line: string): string => plusReg.test(line) ?
+  calculatePlusFirst(line.replace(plusReg, eval)) :
+  line
+
+  console.log('Part 2:', $(input, map(replaceParens(pipe(calculatePlusFirst, calculateFromLeft))), numbers(), sum))
